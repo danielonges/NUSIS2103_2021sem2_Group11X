07cb3609b9869a2f04cc4c991854345c9e6d6698
leeleonard98 <71783105+leeleonard98@users.noreply.github.com>
2021-03-23 14:21:11 +0800

    Added User Entity class

--- EasyAppointmentSystemLibrary/src/entity/CustomerEntity.java
+++ EasyAppointmentSystemLibrary/src/entity/CustomerEntity.java
@@ -6,57 +6,34 @@
 package entity;
 
 import java.io.Serializable;
+import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 /**
  *
  * @author danielonges
  */
 @Entity
-public class CustomerEntity implements Serializable {
+public class CustomerEntity extends UserEntity implements Serializable {
 
     private static final long serialVersionUID = 1L;
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long customerId;
-
+    private String identityNum;
+   
     public CustomerEntity() {
     }
 
-    public Long getCustomerId() {
-        return customerId;
+    public CustomerEntity(String name, String address, String city, String email, String password,String identityNum) {
+        super(name, address, city, email, password);
+        this.identityNum = identityNum;
     }
 
-    public void setCustomerId(Long customerId) {
-        this.customerId = customerId;
-    }
+    
+    
 
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (customerId != null ? customerId.hashCode() : 0);
-        return hash;
-    }
 
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the customerId fields are not set
-        if (!(object instanceof CustomerEntity)) {
-            return false;
-        }
-        CustomerEntity other = (CustomerEntity) object;
-        if ((this.customerId == null && other.customerId != null) || (this.customerId != null && !this.customerId.equals(other.customerId))) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "entity.CustomerEntity[ id=" + customerId + " ]";
-    }
     
 }
