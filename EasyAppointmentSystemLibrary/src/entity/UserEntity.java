18e13d1ae55b064eca98ee363934008f3ca399b3
Daniel Ong <ongfishh@gmail.com>
2021-03-27 17:12:34 +0800

    Update User Entity class

---
+++ EasyAppointmentSystemLibrary/src/entity/UserEntity.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
+/**
+ *
+ * @author leele
+ */
+@Entity
+@Inheritance(strategy=InheritanceType.JOINED)
+
+public class UserEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String address;
+    private String city;
+    private String email;
+    private String password;
+
+    public UserEntity() {
+    }
+
+    public UserEntity(String name, String address, String city, String email, String password) {
+        
+        this.name = name;
+        this.address = address;
+        this.city = city;
+        this.email = email;
+        this.password = password;
+    }
+    
+    
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof UserEntity)) {
+            return false;
+        }
+        UserEntity other = (UserEntity) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.UserEntity[ id=" + id + " ]";
+    }
+    
+}
