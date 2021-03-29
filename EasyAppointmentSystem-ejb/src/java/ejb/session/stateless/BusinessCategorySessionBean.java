ecaa045963e739e835f5029df2035e19742eb287
leeleonard98 <71783105+leeleonard98@users.noreply.github.com>
2021-03-29 12:36:41 +0800

    waeferg

---
+++ EasyAppointmentSystem-ejb/src/java/ejb/session/stateless/BusinessCategorySessionBean.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejb.session.stateless;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author leele
+ */
+@Stateless
+public class BusinessCategorySessionBean implements BusinessCategorySessionBeanRemote, BusinessCategorySessionBeanLocal {
+
+    @PersistenceContext(unitName = "EasyAppointmentSystem-ejbPU")
+    private EntityManager em;
+
+    
+    
+    
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
