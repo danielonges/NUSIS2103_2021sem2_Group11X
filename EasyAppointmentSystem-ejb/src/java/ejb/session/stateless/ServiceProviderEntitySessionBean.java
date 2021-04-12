461572a8181a3b0d16d2daa48397ee1599329da6
Lee Leonard <leeleonard_98@yahoo.com.sg>
2021-04-12 14:43:05 +0800

    exception cases

--- EasyAppointmentSystem-ejb/src/java/ejb/session/stateless/ServiceProviderEntitySessionBean.java
+++ EasyAppointmentSystem-ejb/src/java/ejb/session/stateless/ServiceProviderEntitySessionBean.java
@@ -11,6 +11,7 @@ import java.util.Date;
 import util.exception.InvalidLoginException;
 import util.exception.ServiceProviderNotFoundException;
 import java.util.List;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ejb.EJB;
@@ -21,10 +22,18 @@ import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.NonUniqueResultException;
 import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceException;
 import javax.persistence.Query;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
 import util.enumeration.ServiceProviderStatus;
 import static util.enumeration.ServiceProviderStatus.APPROVE;
 import util.exception.BusinessCategoryNotFoundException;
+import util.exception.InputDataValidationException;
+import util.exception.ServiceProviderAlreadyExistsException;
+import util.exception.UnknownPersistenceException;
 import util.security.CryptographicHelper;
 
 /**
@@ -41,11 +50,51 @@ public class ServiceProviderEntitySessionBean implements ServiceProviderEntitySe
 
     @EJB
     private BusinessCategorySessionBeanLocal businessCategorySessionBeanLocal;
+    private final ValidatorFactory validatorFactory;
+    private final Validator validator;
 
+    public ServiceProviderEntitySessionBean() {
+        validatorFactory = Validation.buildDefaultValidatorFactory();
+        validator = validatorFactory.getValidator();
+    }
+    
+    
     @Override
-    public void createServiceProviderEntity(ServiceProviderEntity newServiceProviderEntity) {
-        em.persist(newServiceProviderEntity);
-        em.flush();
+    public void createServiceProviderEntity(ServiceProviderEntity newServiceProviderEntity) throws ServiceProviderAlreadyExistsException, UnknownPersistenceException, InputDataValidationException {
+        try
+        {
+            Set<ConstraintViolation<ServiceProviderEntity>> constraintViolations = validator.validate(newServiceProviderEntity);
+        
+            if(constraintViolations.isEmpty())
+            {
+                em.persist(newServiceProviderEntity);
+                em.flush();
+
+                
+            }
+            else
+            {
+                throw new InputDataValidationException(prepareInputDataValidationErrorsMessage(constraintViolations));
+            }            
+        }
+        catch(PersistenceException ex)
+        {
+            if(ex.getCause() != null && ex.getCause().getClass().getName().equals("org.eclipse.persistence.exceptions.DatabaseException"))
+            {
+                if(ex.getCause().getCause() != null && ex.getCause().getCause().getClass().getName().equals("java.sql.SQLIntegrityConstraintViolationException"))
+                {
+                    throw new ServiceProviderAlreadyExistsException();
+                }
+                else
+                {
+                    throw new UnknownPersistenceException(ex.getMessage());
+                }
+            }
+            else
+            {
+                throw new UnknownPersistenceException(ex.getMessage());
+            }
+        }
     }
 
     @Override
@@ -208,6 +257,18 @@ public class ServiceProviderEntitySessionBean implements ServiceProviderEntitySe
 //        
         em.persist(ratingEntity);
         em.flush();
+    }
+    
+    private String prepareInputDataValidationErrorsMessage(Set<ConstraintViolation<ServiceProviderEntity>>constraintViolations)
+    {
+        String msg = "Input data validation error!:";
+            
+        for(ConstraintViolation constraintViolation:constraintViolations)
+        {
+            msg += "\n\t" + constraintViolation.getPropertyPath() + " - " + constraintViolation.getInvalidValue() + "; " + constraintViolation.getMessage();
+        }
+        
+        return msg;
     }
 
 }
--- EasyAppointmentSystem-ejb/src/java/ejb/session/stateless/ServiceProviderEntitySessionBeanLocal.java
+++ EasyAppointmentSystem-ejb/src/java/ejb/session/stateless/ServiceProviderEntitySessionBeanLocal.java
@@ -6,11 +6,14 @@ import util.exception.InvalidLoginException;
 import util.exception.ServiceProviderNotFoundException;
 import java.util.List;
 import util.exception.BusinessCategoryNotFoundException;
+import util.exception.InputDataValidationException;
+import util.exception.ServiceProviderAlreadyExistsException;
+import util.exception.UnknownPersistenceException;
 
 public interface ServiceProviderEntitySessionBeanLocal {
     
     
-    public void createServiceProviderEntity(ServiceProviderEntity newServiceProviderEntity);
+    public void createServiceProviderEntity(ServiceProviderEntity newServiceProviderEntity)  throws ServiceProviderAlreadyExistsException, UnknownPersistenceException, InputDataValidationException ;
 
     public ServiceProviderEntity retrieveServiceProviderEntityByProviderId(Long providerId) throws ServiceProviderNotFoundException ;
     
